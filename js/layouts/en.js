5e08d4f0688253c95db9c94dd365dc356849ebc5
George1Meshveliani <giorgi.meshveliani@gmail.com>
2021-10-11 17:53:38 +0400

    init: start project

---
+++ js/layouts/en.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        small: 'CapsLock',
+        shift: null,
+        code: 'CapsLock',
+    },
+    {
+        small: 'a',
+        shift: 'A',
+        code: 'Backquote',
+    },
+    {
+        small: '1',
+        shift: '!',
+        code: 'Digit1',
+    }
+]
---
+++ js/layouts/ru.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        small: 'CapsLock',
+        shift: null,
+        code: 'CapsLock',
+    },
+    {
+        small: 'ё',
+        shift: 'Ё',
+        code: 'Backquote',
+    },
+    {
+        small: '1',
+        shift: '!',
+        code: 'Digit1',
+    }
+]
