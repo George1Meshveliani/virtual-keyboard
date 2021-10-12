65efad8d55ea7db259219c7af5d49d9d7feaccce
George1Meshveliani <giorgi.meshveliani@gmail.com>
2021-10-12 16:55:40 +0400

    feat: create function create

---
+++ js/utils/create.js
@@ -0,0 +1,36 @@
+export default function create(el, classNames, child, parent, ...dataAttr) {
+  let el = null;
+  try {
+    el = document.createElement(el);
+  } catch (error) {
+      throw new Error('Unable to create HTMLelement try again carrefully');
+    }
+
+    if (classNames) element.classList.add(...classNames.split(' '));
+
+    if (child && Array.isArray(child)) {
+        child.forEach((childElement) => childElement && element.appendChild(childElement));
+    } else if (child && typeof child === 'object') {
+        element.appendChild(child);
+    } else if (child && typeof child === 'string') {
+        element.innerHTML = child;
+    }
+
+    if (parent) {
+        parent.appendChild(element);
+    }
+
+    if (dataAttr.length) {
+        dataAttr.forEach(([ attrName, attrValue ]) => {
+            if (attrValue === '') {
+                element.setAttribute(attrName, '');
+            }
+            if (attrName.match(/value|id|placeholder|cols|rows|autocorrect|spellcheck/)) {
+                element.setAttribute(attrName, attrValue);
+            } else {
+                element.dataset[attrName] = attrValue;
+            }
+        });
+    }
+    return element;
+}
