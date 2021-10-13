31d6a549a0a0a81b4866d84d63d5032bd1e5bfff
George1Meshveliani <giorgi.meshveliani@gmail.com>
2021-10-13 11:45:54 +0400

    fix: update styles

--- js/layouts/en.js
+++ js/layouts/en.js
@@ -1,17 +1,17 @@
 export default [
-    {
-        small: 'CapsLock',
-        shift: null,
-        code: 'CapsLock',
-    },
-    {
-        small: 'a',
-        shift: 'A',
-        code: 'Backquote',
-    },
-    {
-        small: '1',
-        shift: '!',
-        code: 'Digit1',
-    }
-]
+  {
+    small: 'CapsLock',
+    shift: null,
+    code: 'CapsLock',
+  },
+  {
+    small: 'a',
+    shift: 'A',
+    code: 'Backquote',
+  },
+  {
+    small: '1',
+    shift: '!',
+    code: 'Digit1',
+  },
+];
--- js/layouts/ru.js
+++ js/layouts/ru.js
@@ -1,17 +1,17 @@
 export default [
-    {
-        small: 'CapsLock',
-        shift: null,
-        code: 'CapsLock',
-    },
-    {
-        small: 'ё',
-        shift: 'Ё',
-        code: 'Backquote',
-    },
-    {
-        small: '1',
-        shift: '!',
-        code: 'Digit1',
-    }
-]
+  {
+    small: 'CapsLock',
+    shift: null,
+    code: 'CapsLock',
+  },
+  {
+    small: 'ё',
+    shift: 'Ё',
+    code: 'Backquote',
+  },
+  {
+    small: '1',
+    shift: '!',
+    code: 'Digit1',
+  },
+];
