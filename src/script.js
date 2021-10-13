3315d5c43316e87af6419f5213f7411ae6763bf4
George1Meshveliani <giorgi.meshveliani@gmail.com>
2021-10-13 18:26:02 +0400

    fix: try different method

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-import KEYS from './keys';
-import Keyboard from './Modules/Keyboard';
+import KEYS from './keys.js';
+import Keyboard from './Modules/Keyboard.js';
 
 window.onload = () => {
   /* Add comment, how to use app and how it was made */
