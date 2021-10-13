98d7150bcb78a2c34263f23f26781e61247e3a3c
George1Meshveliani <giorgi.meshveliani@gmail.com>
2021-10-13 18:48:00 +0400

    delete: remove extra comments

--- src/script.js
+++ src/script.js
@@ -2,25 +2,20 @@ import KEYS from './keys.js';
 import Keyboard from './Modules/Keyboard.js';
 
 window.onload = () => {
-  /* Add comment, how to use app and how it was made */
   const hint = document.createElement('p');
   hint.innerHTML = '<strong> Click left Ctrl + left Shift to change languages</strong>.';
   document.body.append(hint);
 
-  /* Add textarea for to output symbols */
   const textarea = document.createElement('textarea');
   textarea.classList.add('text');
   document.body.append(textarea);
 
-  /* Add keyboard container */
   const keyboardView = document.createElement('div');
   keyboardView.classList.add('keyboard');
   document.body.append(keyboardView);
 
-  /* Create keyboard */
   const keyboard = new Keyboard(KEYS, keyboardView);
 
-  /* What's happening if mouse button'is pressed or released */
   const pressMouseButton = (event) => {
     if (event.target.tagName === 'BUTTON') {
       keyboard.changeState(event.target.dataset.keyCode, event.type);
@@ -33,12 +28,9 @@ window.onload = () => {
     keyboard.changeState(code, event.type);
   };
 
-  /* Catch if mouse button's pressed or released */
   keyboardView.addEventListener('mousedown', pressMouseButton);
   document.addEventListener('click', releaseMouseButton);
-  /* document's using because button can be released when cursor's outside the keyboard area */
 
-  /* What's happening if computer buttons're pressed or released */
   const pressComputerKey = (event) => {
     event.preventDefault();
     const virtualKey = keyboard.view.querySelector(`[data-key-code="${event.code}"]`);
@@ -51,15 +43,12 @@ window.onload = () => {
     }
   };
 
-  /* Catch if computer buttons're pressed or released */
   document.addEventListener('keydown', pressComputerKey);
   document.addEventListener('keyup', pressComputerKey);
 
-  /* Focus on textarea */
   textarea.focus();
   textarea.addEventListener('blur', () => textarea.focus());
 
-  /* Inactivate all buttons (except CapsLock), when focus got out of page and returned back */
   window.addEventListener('blur', () => {
     if (keyboard.pressed.includes('CapsLock')) keyboard.pressed = ['CapsLock'];
     else keyboard.pressed = [];
